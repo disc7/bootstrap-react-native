9d51c7795f8199ae05c55e2a08f4dac83306bbbb
Charlie Abbott <charlie.abbott@gmail.com>
2017-10-17 16:15:15 -0500

    Adding fonts

--- App/App.js
+++ App/App.js
@@ -1,34 +1,119 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 import Input from './components/input/Input.js';
 import TinderCards from './components/tinder/Tinder.js';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
   state = {
+    fontsLoaded: false,
     value: '',
     jokes: []
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'droid-sans-bold': require('./assets/fonts/DroidSans-Bold.ttf'),
+      'droid-sans': require('./assets/fonts/DroidSans-Regular.ttf'),
+    });
+    this.setState({ fontsLoaded: true });
+    console.log('async componentDidMount')
+  }
+  handlePress() {
+    console.log('handlePress')
   }
   render() {
     return (
       <View style={styles.container}>
-        <Input
-          updateJokes={(jokes) => this.setState({jokes: jokes})}
-          value={this.state.value}
-          onChange={(value) => this.setState({value: value})}
-        />
-        <TinderCards
-          jokes={this.state.jokes}
-        />
+        <View style={styles.boxTop}>
+          {
+            this.state.fontsLoaded ? <Text style={styles.textTitleBold}>DAD JOKES <Text style={styles.textTitle}>APP</Text></Text> : null
+          }
+        </View>
+        <View style={styles.boxMiddle} />
+        <View style={styles.boxBottom}>
+          <View style={styles.boxBottomContainer}>
+            <TouchableHighlight onPress={this.handlePress} style={styles.buttonHighlight}>
+              <View style={styles.button1} />
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.handlePress} style={styles.buttonHighlight}>
+              <View style={styles.button2} />
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.handlePress} style={styles.buttonHighlight}>
+              <View style={styles.button3} />
+            </TouchableHighlight>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
+let cicleButtonLargeSize = 50;
+let cicleButtonSmallSize = 40;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'stretch',
+  },
+  boxTop: {
+    flex: 0,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 10,
+    backgroundColor: 'powderblue',
+  },
+  boxMiddle: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  boxBottom: {
+    flex: 0,
+    alignItems: 'center',
+    backgroundColor: 'whitesmoke',
+  },
+  boxBottomContainer: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 90,
+    width: 220,
+  },
+  buttonHighlight: {
+   borderRadius: cicleButtonLargeSize/2,
+  },
+  button1: {
+    width: cicleButtonLargeSize,
+    height: cicleButtonLargeSize,
+    borderRadius: cicleButtonLargeSize/2,
+    borderColor: 'gainsboro',
+    borderWidth: 2,
+    backgroundColor: 'white',
+  },
+  button2: {
+    width: cicleButtonSmallSize,
+    height: cicleButtonSmallSize,
+    borderRadius: cicleButtonSmallSize/2,
+    borderColor: 'gainsboro',
+    borderWidth: 2,
+    backgroundColor: 'white',
+  },
+  button3: {
+    width: cicleButtonLargeSize,
+    height: cicleButtonLargeSize,
+    borderRadius: cicleButtonLargeSize/2,
+    borderColor: 'gainsboro',
+    borderWidth: 2,
+    backgroundColor: 'white',
+  },
+  textTitleBold: {
+    fontFamily: 'droid-sans-bold',
+    fontSize: 20,
+  },
+  textTitle: {
+    fontFamily: 'droid-sans',
+    fontSize: 20,
   }
 });
